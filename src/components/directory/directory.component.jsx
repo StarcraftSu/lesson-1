d90c1af5a44a86668aad24c89c71cc4ced32dd36
StarcraftSu <starcraftsu@gmail.com>
2020-01-12 17:07:32 +0800

    reconstruct this app

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+import React,{ useState } from 'react'
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.style.scss'
+import dummyData from './dummyData.json'
+const DirectoryMenu = ()=>{
+    const [sections,setSections] = useState(dummyData)
+    return(
+        <div className="directory-menu">
+            {
+                sections.map(item=>(
+                    <MenuItem {...item}/>
+                ))
+            }
+        </div>
+    )
+}
+
+export default DirectoryMenu
