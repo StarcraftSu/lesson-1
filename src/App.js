cbe935390a7975c31c764ee761412ca40714a876
StarcraftSu <starcraftsu@gmail.com>
2020-01-12 15:30:38 +0800

    add node-sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import HomePage from './homepage.component'
 class App extends Component {
   render() {
-    return <div className='App'>Starcraft Su</div>;
+    return (
+      <div className='App'>
+        <HomePage/>
+      </div>
+    );
   }
 }
 
