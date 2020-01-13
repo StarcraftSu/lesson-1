11f6a4faaf4545b24ba31f938f218f071144e8ca
StarcraftSu <starcraftsu@gmail.com>
2020-01-13 22:50:31 +0800

    add sign-in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'; //react-router-dom contains re
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import Header from './components/header/header.component'
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInAndSignUpPage}></Route>
         </Switch>
       </div>
     );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './sign-in.style.scss'
+
+class SignIn extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email:'',
+            password:''
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+
+        this.setState(
+            {
+                email:'',
+                password:''
+            }
+        )
+    }
+
+    handleChange = e =>{
+        const { value, name } = e.target
+        this.setState({
+            [name]:value
+        })
+    }
+
+    render(){
+        const {email,password} = this.state
+        return(
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="email" value={email} onChange={this.handleChange} required/>
+                    <label>Email</label>
+                    <input type="password" name="password" value={password} onChange={this.handleChange} required/>
+                    <label>Password</label>
+
+                    <input type="sumit" value="Submit Form"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn
